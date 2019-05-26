78fc2cdde6734fafd219815822f1c5753a2308d5
Mathias Bischoff <mathiasbischo@gmail.com>
2019-05-26 14:47:45 +0200

    exercise02

---
+++ CartesianProduct.py
@@ -0,0 +1,21 @@
+
+
+
+set1 = ("a","b","c") #set1
+set2 = (1,2,3,4) #set2
+
+def CartesianProduct(set1, set2):
+    print("CARTESIAN PRODUCT OF:")
+    print("AxB = {", end="")
+    for i in range(len(set1)):
+        for j in range(len(set2)):
+            print("(",set1[i],",",set2[j],")", end ="")
+    print("}")
+
+#def BinaryRelation(set1, set2):
+
+if __name__ == "__main__":
+    set1 = ("a","b","c") #set1
+    set2 = (1,2,3,4) #set2
+
+    CartesianProduct(set1,set2)
